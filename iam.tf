a20c6e6d5c508859e5221aabf9b5ae2e8a834829
HenriqueSydney <henriquesydney@hotmail.com>
2024-09-30 23:27:27 -0300

    New role

--- iam.tf
+++ iam.tf
@@ -13,6 +13,35 @@ resource "aws_iam_openid_connect_provider" "oidc-git" {
 
 }
 
+resource "aws_iam_role" "tf-role" {
+  name = "rf-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = "sts:AssumeRoleWithWebIdentity"
+        Principal = {
+          Federated = "arn:aws:iam::585445949187:oidc-provider/token.actions.githubusercontent.com"
+        }
+        Condition = {
+          StringEquals = {
+            "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
+            "token.actions.githubusercontent.com:sub" = "repo:HenriqueSydney/projeto-iac:ref:refs/heads/main"
+          }
+        }
+      }
+    ]
+  })
+
+
+  tags = {
+    Iac = true
+  }
+
+}
+
 resource "aws_iam_role" "app-runner-role" {
   name = "app-runner-role"
 
