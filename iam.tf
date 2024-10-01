d17177c706bdf8b8a4a1020d9a44e30070edf9f3
HenriqueSydney <henriquesydney@hotmail.com>
2024-09-30 23:16:33 -0300

    first commit

---
+++ iam.tf
@@ -0,0 +1,113 @@
+resource "aws_iam_openid_connect_provider" "oidc-git" {
+  url = "https://token.actions.githubusercontent.com"
+
+  client_id_list = [
+    "sts.amazonaws.com",
+  ]
+
+  thumbprint_list = ["d89e3bd43d5d909b47a18977aa9d5ce36cee184c"]
+
+  tags = {
+    Iac = true
+  }
+
+}
+
+resource "aws_iam_role" "app-runner-role" {
+  name = "app-runner-role"
+
+  assume_role_policy = jsonencode({
+    Version : "2012-10-17",
+    Statement : [
+      {
+        Effect : "Allow",
+        Principal : {
+          Service : "build.apprunner.amazonaws.com"
+        },
+        Action : "sts:AssumeRole"
+      }
+    ]
+  })
+
+  managed_policy_arns = [
+    "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  ]
+
+  tags = {
+    Iac = true
+  }
+}
+
+resource "aws_iam_role" "ecr_role" {
+  name = "ecr_role"
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = "sts:AssumeRoleWithWebIdentity"
+        Principal = {
+          Federated = "arn:aws:iam::585445949187:oidc-provider/token.actions.githubusercontent.com"
+        }
+        Condition = {
+          StringEquals = {
+            "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
+            "token.actions.githubusercontent.com:sub" = "repo:HenriqueSydney/desafio-docker:ref:refs/heads/main"
+          }
+        }
+      }
+    ]
+  })
+
+  inline_policy {
+    name = "ecr-app-permission"
+
+    policy = jsonencode({
+      Version = "2012-10-17"
+      Statement = [
+        {
+          Sid      = "Statement1"
+          Action   = "apprunner:*",
+          Effect   = "Allow",
+          Resource = "*"
+
+        },
+        {
+          Sid      = "Statement2"
+          Action   = [
+            "iam:PassRole",
+            "iam:CreateServiceLinkedRole"
+          ],
+          Effect   = "Allow",
+          Resource = "*"
+
+        },
+        {
+          Sid = "Statement3"
+          Action = [
+            "ecr:GetDownloadUrlForLayer",
+            "ecr:BatchGetImage",
+            "ecr:BatchCheckLayerAvailability",
+            "ecr:PutImage",
+            "ecr:InitiateLayerUpload",
+            "ecr:UploadLayerPart",
+            "ecr:CompleteLayerUpload",
+            "ecr:GetAuthorizationToken",
+          ]
+          Effect   = "Allow"
+          Resource = "*"
+        },
+      ]
+    })
+  }
+
+
+  tags = {
+    Iac = true
+  }
+}
+
+
